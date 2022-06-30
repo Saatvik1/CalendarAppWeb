79b6c09d9191b70e67c307ab6f0ea94bb340bd21
Saatvik1 <saatviks3723@gmail.com>
2022-06-29 23:38:43 -0400

    Add more functionality to the calendar. Added a feature where when the user clicks on a date, there will be a pop up and it auto fills the dates to the clicked date and the next date after.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CalendarAppWeb
+
+IF user clicks once on the date, it will open a window to add 
+all the elements to make an event and when click add or delete on that 
+popup window
+
+if user double clicks, they can see the hourly time period for that day 
+there they can add events, see current events, and edit events. 
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -42,6 +42,10 @@ const renderCalendar = () => {
     "December",
   ];
 
+  function getMonthString(compare){
+  return months.findIndex("June") + 1;
+  }
+
   document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
   document.querySelector(".date p").innerHTML = new Date().toDateString();
@@ -49,7 +53,7 @@ const renderCalendar = () => {
   let days = "";
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    days += `<div class="prev-date" id= "${x} prev">${prevLastDay - x + 1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++) {
@@ -57,14 +61,14 @@ const renderCalendar = () => {
       i === new Date().getDate() &&
       date.getMonth() === new Date().getMonth()
     ) {
-      days += `<div class="today">${i}</div>`;
+      days += `<div class="today" id= "${i} norm">${i}</div>`;
     } else {
-      days += `<div>${i}</div>`;
+      days += `<div id= "${i} norm">${i}</div>`;
     }
   }
 
   for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date">${j}</div>`;
+    days += `<div class="next-date" id= "${j} next">${j}</div>`;
     monthDays.innerHTML = days;
   }
 };
@@ -79,4 +83,102 @@ document.querySelector(".next").addEventListener("click", () => {
   renderCalendar();
 });
 
+
+let dayIdClicked = "";
+
+function stringGenerator1(monthCon, dateCon){
+let finalString = "";
+
+finalString += date.getFullYear() + "-";
+
+ if(monthCon.length == 2){
+ finalString += monthCon + "-";
+ } else {
+ finalString += "0" + monthCon + "-";
+ }
+
+if(dateCon.length == 2){
+finalString += dateCon;
+} else {
+finalString += "0" + dateCon;
+}
+
+return finalString;
+}
+
+function stringGenerator2(monthCon, dateCon){
+let finalString = "";
+
+finalString += date.getFullYear() + "-";
+
+
+var integerTestDate = parseInt(dateCon, 10);
+var integerTestMonth = parseInt(monthCon, 10);
+
+if(integerTestDate + 1 > new Date(date.getFullYear(),date.getMonth() + 1,0).getDate()){
+dateCon = 1;
+integerTestMonth += 1;
+monthCon = integerTestMonth + "";
+} else {
+integerTestDate += 1;
+dateCon = integerTestDate + "";
+}
+
+ if(monthCon.length == 2){
+ finalString += monthCon + "-";
+ } else {
+ finalString += "0" + monthCon + "-";
+ }
+
+if(dateCon.length == 2){
+finalString += dateCon;
+} else {
+finalString += "0" + dateCon;
+}
+
+return finalString;
+}
+
+const onClick = (event) => {
+    let dayIdClicked = event.srcElement.id;
+    let popup = document.getElementById("popup");
+    popup.classList.add("open-popup");
+    let startDate = document.getElementById("set-event-start-date");
+    let endDate = document.getElementById("set-event-end-date");
+
+    let dayInput = dayIdClicked.replace(" norm", "");
+    let monthInput = 0;
+
+  let compare = document.getElementById("rips").innerText;
+
+
+const months = [
+    "JANUARY",
+    "FEBRUARY",
+    "MARCH",
+    "APRIL",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUGUST",
+    "SEPTEMBER",
+    "OCTOBER",
+    "NOVEMBER",
+    "DECEMBER",
+  ];
+
+ monthInput = months.findIndex(month => month === compare) + 1;
+
+  stringGenerator1(monthInput,dayInput);
+
+  startDate.value = stringGenerator1(monthInput, dayInput);
+  endDate.value = stringGenerator2(monthInput, dayInput);
+}
+
+document.querySelector(".days").addEventListener("click", onClick);
+
 renderCalendar();
+
+//Display the year next to the month
+//Fix so that if clicked off a date it returns as null. and set it to the current date and the next date.
+//Add so that if user clicks on next months date the calendar switches to the next month.
